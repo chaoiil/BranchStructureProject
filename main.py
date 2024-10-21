9716510374e34e16702c9d691983d0ce1fce5c34
chaoiil <n.chaoil976@gmail.com>
2024-10-21 13:00:29 +0200

    vyvghv g

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 print ("salut")
 
 def addition(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Les arguments doivent être des nombres (int ou float).")
     return a + b
